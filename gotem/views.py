d94664fcca3b5dafcc069baad5ddd9bb8d4fb88b
Jordi Sala <jordism91@gmail.com>
2013-04-21 22:20:09 +0200

    Added 404 for routes if not accessed via ajax

--- gotem/views.py
+++ gotem/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 from django.utils import simplejson as json
 from django.http import Http404, HttpResponse, HttpResponseBadRequest
-from models import Objective, ObjectiveForm
+from models import Objective
 
 def index(request):
     return render(request, 'gotem/index.html')
 
 def objective_list(request):
-    objectives = Objective.objects.all()
-    context = { 'objectives' : objectives }
-    return render(request, 'gotem/sprint-area/objective-list.html', context)
+    if request.is_ajax():
+        objectives = Objective.objects.all()
+        context = { 'objectives' : objectives }
+        return render(request, 'gotem/sprint-area/objective-list.html', context)
+    else:
+        raise Http404
 
 def objective_new(request):
     if request.is_ajax():
