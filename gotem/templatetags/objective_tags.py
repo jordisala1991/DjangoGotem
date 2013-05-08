fda679a722098a7203adbc7ea855c9217387a882
Jordi Sala <jordism91@gmail.com>
2013-05-08 23:48:17 +0200

    deleted some files

--- gotem/templatetags/objective_tags.py
+++
@@ -1,9 +0,0 @@
-from django import template
-from gotem.models import ObjectiveForm
-
-register = template.Library()
-
-@register.inclusion_tag('gotem/sprint-area/objective-list__controls.html')
-def objective_form():
-	form = ObjectiveForm()
-	return { 'form' : form }
