47d35fce67a7fb0fe876c88af1fe3c53154e8d94
Jordi Sala <jordism91@gmail.com>
2013-05-31 18:24:06 +0200

    added new files to repository

--- app/settings.py
+++ app/settings.py
@@ -15,10 +15,10 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'db',                      # Or path to database file if using sqlite3.
+        'NAME': 'db', # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'root',
-        'PASSWORD': 'runroom,2005',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
@@ -124,9 +124,9 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'gotem',
     'south',
     'compressor',
-    'gotem',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
--- static/CACHE/js/4be463a3e0cd.js
+++
@@ -1,1 +0,0 @@
-(function(){var square;square=function(x){return x*x;};}).call(this);
